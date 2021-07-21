fc0d8237a827754336d86c0f0d4290a286069954
Andres-RG <lrojas26@alumnos.uaq.mx>
2021-07-21 17:31:01 -0500

    codigo para la salida de las graficas

--- 02_Scripts/Scripst/Scrpts.R
+++ 02_Scripts/Scripst/Scrpts.R
@@ -1,2 +1,10 @@
 source("02_Scripts/Funciones/Funciones_utiles.R")
 cuadratica(2, 10,6)
+
+
+# Para guardar las gráficas en el directorio de Output
+# Generar la grafica con el codigo y gaurdarla en un objeto
+# ...
+# pdf("03_Output/Plots/Nombre_de_la_grafica.pdf")
+# objeto de la grafica
+# dev.off()
